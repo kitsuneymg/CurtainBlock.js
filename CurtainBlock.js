499ebbca516fe4745e45fd9e66f41bde648c586c
kitsuneymg <kitsuneymg@gmail.com>
2014-03-02 12:08:56 -0600

    Update CurtainBlock.js
    Tumblr only
    Activates iff a post tagged #nsfw is present. Otherwise, behave exactly like curtain-block does

--- CurtainBlock.js
+++ CurtainBlock.js
@@ -1,24 +1,40 @@
 // CurtainBlock.js
 // Written by Jessie Mills
-// Setup (for easy dev access)
+// Edited by Kittyhawk Contrail
 function CurtainBlock(persistent,transparency,redir) {
 $(function() {
 	$curtain = $('.CurtainBlock');
 	$alert = $('.CurtainBlockAlert');
 	$confirm = $('.CurtainBlockConfirm');
 	$deny = $('.CurtainBlockDeny');
+	$curl = $(location).attr('href');
 	complete = 0;
+	
+	    //check to see if we're on a 'safe' tagged page
+        allsafe=0;
+        $('.tags').each( function(index, element ){
+            allsafe=1;
+            if( $(element).children('a:contains("nsfw")').length > 0) {
+                allsafe=0;
+                return false;
+            }
+        } )
+        if (allsafe) {
+            $curtain.hide();
+            complete = 1; persistent = 0;
+        }
+	    
 		// Checks for localStorage and disables the curtain, if that option is toggled.
 		if (persistent) {
 			if (localStorage.getItem("curtainConfirm")) {
 				$curtain.hide();
 				complete = 1;
-			}
-		}
+			};
+		};
 		// If we see that stored key, run the Curtain.
 		if (complete === 0) {
 			$confirm.show();
-			$deny.show();
+   			$deny.show();
 			// Redirect / hide based on user input
 			if (transparency) {
 				$curtain.css('background-color','rgba(0,0,0,0.85');
@@ -29,13 +45,12 @@ $(function() {
 				// If we're persistent, set up the localStorage
 				if (persistent) {
 					localStorage.setItem("curtainConfirm", true);
-				}
+				};
 			});
 			$deny.click(function () {
 				// Redir function
 				window.location.replace(redir);
 			});
 		}
-	}
-);
-}
+	})
+};
